53a94706fe7bbb7406c914b76fafe381fed0d5d1
Jason Chalifour <jwchalifour@mac.com>
2025-05-18 07:36:17 -0600

    changed submit icon

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -613,7 +613,7 @@ export default function HomePage() {
                     </svg>
                 ) : (
                     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-5 h-5 mr-2">
-                         <path strokeLinecap="round" strokeLinejoin="round" d="m15.75 15.75-2.489-2.489m0 0a3.375 3.375 0 1 0-4.773-4.773 3.375 3.375 0 0 0 4.774 4.774ZM21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z" />
+                         <path strokeLinecap="round" strokeLinejoin="round" d="M12 4.5v15m7.5-7.5h-15" />
                     </svg>
                 )}
                 Add Meal
